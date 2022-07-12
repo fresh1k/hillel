86734e1c1fdc3040fb35ea774d6ecefea3d54039
fresh1k <s3mpLe@gmail.com>
2022-07-12 20:40:55 +0300

    zad1 is not finished, zad2 i zad 3 are finished

---
+++ Lesson-4/main.js
@@ -0,0 +1,48 @@
+// Zadanie 1
+// let arr1 = [3, 45, 23, 78, 34];
+// let arr2 = [4, 2, 34, 4, 12, 1];
+
+// let arr3 = [];
+
+
+
+// for (let i = 0; i < arr1.length; i++) {
+//     arr3.push(arr1[i] * arr2[i]);
+// }
+
+// console.log(arr3);
+
+
+// Zadanie 2
+
+// let arr = [4, 2, 34, 4, 1, 12, 1, 4, 34, 56, 12];
+
+// let arr1 = [];
+
+// let result = null;
+
+// for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < i; j++) {
+//         if (arr[i] === arr[j]) {
+//             arr1.push(arr[i]);
+//             result = [...new Set(arr1)];
+//         }
+//     }
+// }
+// console.log(result);
+
+// Zadanie 3
+
+// let word = prompt('Введите слово:');
+// let letter = prompt('Введите искомую букву:');
+
+
+// let counter = 0;
+
+// for (let i = 0; i < word.length; i++) {
+//     if (word[i].includes(letter)) {
+//         counter++;
+//     }
+// }
+
+// console.log('The string contains the following number of' + ' ' + `${letter}` + ' ' + 'letter:', `${counter}`);
