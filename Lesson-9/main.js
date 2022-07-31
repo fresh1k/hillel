dc3100c9a407cc65a8c4ed039f91e8d1dc1a9ac2
fresh1k <s3mpLe@gmail.com>
2022-07-31 12:19:29 +0300

    ADD 2 realization

--- Lesson-9/main.js
+++ Lesson-9/main.js
@@ -64,34 +64,6 @@ document.write(
                 <th>Price</th>
             </tr>`
 )
-// function Product(category, type, price) {
-// this.category = category;
-// this.type = type;
-// this.price = price;
-// this.render = function () {
-// return category.forEach(product => {
-// document.write(
-// `<tr class="products">
-// <td><img src="images/${this.name}/${product.type}.svg" alt="${product.type}" width="50" height="50"></td>
-// <td>${product.type}</td>
-// <td><b>${Array.isArray(product.price) ? `${product.price[0]} - ${product.price[product.price.length - 1]}` : product.price} USD</b></td>
-// </tr>`
-// )
-// });
-// }
-// }
-
-// let mebel = new Product(furniture);
-// mebel.name = 'furniture';
-// mebel.render();
-
-// let device = new Product(devices);
-// device.name = 'devices';
-// device.render();
-
-// let appliance = new Product(appliances)
-// appliance.name = 'appliances';
-// appliance.render();
     
     
 function Product(category, type, price) {
@@ -108,38 +80,55 @@ function Product(category, type, price) {
     }
 }
 
-const furniture1 = new Product('furniture', 'desk', 27.87);
-const furniture2 = new Product('furniture', 'closet', 187.32);
-const furniture3 = new Product('furniture', 'chair', 79.29);
-const furniture4 = new Product('furniture', 'bed', 239.99);
-const devices1 = new Product('devices', 'desktop', [272, 769]);
-const devices2 = new Product('devices', 'laptop', [499, 1800]);
-const devices3 = new Product('devices', 'smartphone', [100, 800]);
-const devices4 = new Product('devices', 'tablet', [185, 1000]);
-const devices5 = new Product('devices', 'console', 889);
-const appliances1 = new Product('appliances', 'oven', 780);
-const appliances2 = new Product('appliances', 'microwave', [50, 1400]);
-const appliances3 = new Product('appliances', 'mixer', 215.17);
+// РЕАЛИЗАЦИЯ ЧЕРЕЗ ФУНКЦИЮ
 
-document.write(
-    furniture1.render(),
-    furniture2.render(),
-    furniture3.render(),
-    furniture4.render(),
-    devices1.render(),
-    devices2.render(),
-    devices3.render(),
-    devices4.render(),
-    devices5.render(),
-    appliances1.render(),
-    appliances2.render(),
-    appliances3.render(),
-)
+// function Category(array, name) {
+//     array.forEach(element => {
+//         let product = new Product(name, element.type, element.price)
+//         document.write(product.render())
+//     })
+//     return array;
+// }
 
-console.log(furniture4.category); // furniture
-console.log(furniture4.type); // bed
-console.log(furniture4.price); // 239.99
-console.log(furniture4.render());
+// let product1 = new Category(furniture, 'furniture');
+// let device1 = new Category(devices, 'devices')
+// let appliances1 = new Category(appliances, 'appliances')
+
+
+// РЕАЛИЗАЦИЯ БЕЗ ФУНКЦИИ
+
+// const furniture1 = new Product('furniture', 'desk', 27.87);
+// const furniture2 = new Product('furniture', 'closet', 187.32);
+// const furniture3 = new Product('furniture', 'chair', 79.29);
+// const furniture4 = new Product('furniture', 'bed', 239.99);
+// const devices1 = new Product('devices', 'desktop', [272, 769]);
+// const devices2 = new Product('devices', 'laptop', [499, 1800]);
+// const devices3 = new Product('devices', 'smartphone', [100, 800]);
+// const devices4 = new Product('devices', 'tablet', [185, 1000]);
+// const devices5 = new Product('devices', 'console', 889);
+// const appliances1 = new Product('appliances', 'oven', 780);
+// const appliances2 = new Product('appliances', 'microwave', [50, 1400]);
+// const appliances3 = new Product('appliances', 'mixer', 215.17);
+
+// document.write(
+//     furniture1.render(),
+//     furniture2.render(),
+//     furniture3.render(),
+//     furniture4.render(),
+//     devices1.render(),
+//     devices2.render(),
+//     devices3.render(),
+//     devices4.render(),
+//     devices5.render(),
+//     appliances1.render(),
+//     appliances2.render(),
+//     appliances3.render(),
+// )
+
+// console.log(furniture4.category); // furniture
+// console.log(furniture4.type); // bed
+// console.log(furniture4.price); // 239.99
+// console.log(furniture4.render());
 
 
 
@@ -164,13 +153,3 @@ document.write(
     
     
     // 33087,23 + 11845,07 + 671,22
-
-
-    // this.render = function() {
-    //     return `
-    //         <tr class="products">
-    //             <td><img src="images/${this.category}/${this.type}.svg" alt="${this.type}" width="50" height="50"></td>
-    //             <td>${this.type}</td>
-    //             <td><b>${this.price} USD</b></td>
-    //         </tr>`
-    // }
