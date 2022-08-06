4242aa7eedcf1024b89f42c5feb80c5b8920f1e7
fresh1k <s3mpLe@gmail.com>
2022-08-06 12:16:24 +0300

    Homework-11-Complete

---
+++ Lesson-11/main.js
@@ -0,0 +1,76 @@
+const HAMBURGER =  
+    {
+        'little': {
+            'price': 5,
+            'callories': 20,
+        },
+        'big': {
+            'price': 10,
+            'callories': 40,
+        },
+        'topping': {
+            'cheese': {
+                'price': 1,
+                'callories': 20,
+            },
+            'salad': {
+                'price': 2,
+                'callories': 5,
+            },
+            'potato': {
+                'price': 1.5,
+                'callories': 10,
+            }
+        },
+        'supplements': {
+            'seasoning': {
+                'price': 1.5,
+                'callories': 0,
+            },
+            'mayonnaise': {
+                'price': 2,
+                'callories': 5,
+            }
+        }
+}
+
+
+class Hamburger{
+    constructor(size) {
+        this.size = size;
+        this.toppings = [];
+        this.supplement = [];
+    }
+    addToppings(arr) {
+        arr.forEach(element => {
+            this.toppings.push(element)
+        });
+        return console.log(arr)
+    }
+    addSupplement(arr) {
+        arr.forEach(el => {
+            this.supplement.push(el)
+        })
+        return console.log(arr);
+    }
+
+    getPrice() {
+        let price = this.size.price;
+        this.toppings.forEach(item => price += item.price)
+        this.supplement.forEach(item => price += item.price)
+        return console.log(price)
+    }
+
+    getCalories() {
+        let callories = this.size.callories;
+        this.toppings.forEach(item => callories += item.callories)
+        this.supplement.forEach(item => callories += item.callories)
+        return console.log(callories)
+    }
+}
+
+const burger = new Hamburger(HAMBURGER.little);
+burger.addToppings([HAMBURGER.topping.potato, HAMBURGER.topping.salad]);
+burger.addSupplement([HAMBURGER.supplements.mayonnaise, HAMBURGER.supplements.seasoning]);
+burger.getPrice();
+burger.getCalories();
