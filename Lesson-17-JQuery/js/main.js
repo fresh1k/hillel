b830bcfa6065dda213b6aaaf297a7d51539be461
fresh1k <s3mpLe@gmail.com>
2022-09-04 12:10:59 +0300

    second-version

--- Lesson-17-JQuery/js/main.js
+++ Lesson-17-JQuery/js/main.js
@@ -2,46 +2,27 @@ $(document).ready(function () {
     const films = $('#films');
     const film_detail = $('#film-detail');
     const films_array = [];
-    
-    $(window).on("load", function () {
-        $.ajax({
-            type: "GET",
-            url: "https://swapi.dev/api/films/",
-            dataType: "json",
-            async: "true",
-            success: function (response) {
-                displayFilms(response.results)
-                films_array.push(response.results)
-            }
-        });
-    });
 
 
-    const displayFilms = (film) => {
-        const htmlString = film.map(f => {
-            return `<div class="film">
-                        <a href="#" class="film-link"><img class="film-poster" src="images/${f.title}.jpg" alt="film-poster"></a>
+    const displayFilm = (film) => {
+            return films.html(`<div class="film">
+                        <a href="#" class="film-link"><img class="film-poster" src="images/${film.title}.jpg" alt="film-poster"></a>
                         <div class="film-description">
-                        <h1 class="film-title"><a class="film-link" href="#">${f.title}</a></h1>
-                            <p class="film-year">${f.release_date}</p>
+                        <h1 class="film-title"><a class="film-link" href="#">${film.title}</a></h1>
+                            <p class="film-year">${film.release_date}</p>
                         </div>
-                    </div>`;
-        }).join('');
-        films.html(htmlString)
+                    </div>`);
     }
 
     const filmDetail = (movie) => {
-        const str = movie.map(m => {
-            return  `<div class="film">
-                        <div class="film-description">
-                            <p class="film-year">${m.release_date}</p>
-                            <p class="film-year">${m.opening_crawl}</p>
-                            <p class="film-year">${m.director}</p>
-                            <p class="film-year">${m.producer}</p>
-                        </div>
-                    </div>`
-        }).join('');
-        film_detail.html(str)
+        return film_detail.html(`<div class="film">
+                    <div class="film-description">
+                        <p class="film-year">${movie.release_date}</p>
+                        <p class="film-year">${movie.opening_crawl}</p>
+                        <p class="film-year">${movie.director}</p>
+                        <p class="film-year">${movie.producer}</p>
+                    </div>
+                </div>`);
     }
 
     $('#search').keyup(function () {
@@ -51,8 +32,10 @@ $(document).ready(function () {
             url: `https://swapi.dev/api/films/?search=${searchString}`,
             dataType: "json",
             success: function (response) {
-                displayFilms(response.results)
-                filmDetail(response)
+                displayFilm(response.results[0])
+                $('.film-link').on('click', function () {
+                    filmDetail(response.results[0])
+                })
             }
         })
         // Список ниже более универсальнее чем с ajax 
